bc8d081669aaa820c13b9751f87b62bd7bdbbe37
luizfelipe-git <luizf.892002@gmail.com>
2026-09-11 23:39:03 -0300

    Exercício 007 - Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.

---
+++ Exercicios/Etapa 02 - Tratando dados e fazendo contas/ex007.py
@@ -0,0 +1,5 @@
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+media = (n1 + n2) / 2
+
+print("A média das notas {} e {} é igual a {:.1f}.".format(n1, n2, media))
